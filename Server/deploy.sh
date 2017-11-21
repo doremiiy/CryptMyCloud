be2c242babdc334dfe4aa4d8f794fe3b9acef0ca
Gabrielle Spielmann <gabrielle.spielmann@etu.utc.fr>
2017-11-22 00:16:51 +0100

    apache configuration & ssl

---
+++ Server/deploy.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+# Warning: script sould be launched as root user
+
+# software setup
+apt-get update
+apt-get install python3 python3-pip apache2 libapache2-mod-wsgi-py3
+mkvirtualenv sr04_server --python=python3
+
+# Project repo setup
+git clone https://gitlab.utc.fr/divitare/CryptMyCloud.git
+rm -rf CryptMyCloud/PythonClient/
+mv CryptMyCloud/Server/* CryptMyCloud/
+rm -rf CryptMyCloud/Server/
+cd CryptMyCloud
+
+# Django setup
+pip install -r requirements.txt
+python CryptMyCloud/manage.py migrate
+python CryptMyCloud/manage.py collectstatic
+
+# Database setup
+chmod g+w CryptMyCloud/db.sqlite3
+chown :www-data CryptMyCloud/db.sqlite3
+
+chmod g+w CryptMyCloud
+chown :www-data CryptMyCloud
+
+# Apache setup
+cat > /etc/apache2/sites-available/000-default.conf <<EOL
+<VirtualHost *:80>
+    ServerAdmin admin@example.com
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+    # This is optional, in case you want to redirect people
+    # from http to https automatically.
+    RewriteEngine On
+    RewriteCond %{HTTPS} off
+    RewriteRule (.*) https://%{SERVER_ADDR}/%{REQUEST_URI}
+
+</VirtualHost>
+EOL
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/certs/cryptmycloud.key -out /etc/apache2/certs/cryptmycloud.crt
+
+cat > /etc/apache2/sites-available/default-ssl.conf <<EOL
+<VirtualHost *:443>
+        ServerAdmin webmaster@localhost
+        DocumentRoot /var/www/html
+
+        ErrorLog ${APACHE_LOG_DIR}/error.log
+        CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+        SSLEngine on
+        SSLCertificateFile /etc/apache2/certs/cryptmycloud.crt
+        SSLCertificateKeyFile /etc/apache2/certs/cryptmycloud.key
+
+        <Directory /home/pi/CryptMyCloud/server>
+                <Files wsgi.py>
+                        Require all granted
+                </Files>
+        </Directory>
+
+        Alias /static/ /home/pi/CryptMyCloud/static/
+
+        <Directory /home/pi/CryptMyCloud/static>
+                Options Indexes FollowSymLinks
+                AllowOverride None
+                Require all granted
+        </Directory>
+
+        WSGIDaemonProcess CryptMyCloud python-home=/home/pi/.virtualenvs/sr04_server/ python-path=/home/pi/CryptMyCloud
+        WSGIProcessGroup CryptMyCloud
+        WSGIScriptAlias / /home/pi/CryptMyCloud/server/wsgi.py
+        WSGIPassAuthorization On
+</VirtualHost>
+EOL
+
+a2ensite default-ssl
+a2enmod rewrite  # redirection
+a2enmod ssl  # ssl
+
+service apache2 restart  # restart apache
+
+# source /etc/apache2/envvars
